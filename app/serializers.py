e12fcabdbed4adb6987f6dead6dae37710561959
SHARVAN223 <sharvan70458@gmail.com>
2026-03-22 23:21:03 +0530

    deserializer

--- app/serializers.py
+++ app/serializers.py
@@ -1,11 +1,30 @@
 from rest_framework import serializers
+from app.models import Student
 
 class Stu_serializer(serializers.Serializer):
     Name = serializers.CharField(max_length=50)
     Age = serializers.IntegerField()
     City = serializers.CharField(max_length=50)
-    Contact = serializers.CharField(max_length=15)
+    Contact = serializers.CharField(max_length=15)\
+    
 
+
+    def create(self,validated_data):
+        return Student.objects.create(**validated_data)
+
+
+    def update(self, instance,validated_data):
+        instance.Name = validated_data.get('name',instance.Name)
+        instance.Age = validated_data.get('age',instance.Age)
+        instance.Contact = validated_data.get('contact',instance.Contact)
+        instance.City = validated_data.get('city',instance.City)
+        instance.save()
+        return instance
+
+
+
+
+    
 
 
     
--- app/views.py
+++ app/views.py
@@ -1,13 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from app.models import Student as stu
 from app.serializers import Stu_serializer 
 from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+import io
+from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
 def student(req):
+    if req.method == 'POST':
+        data = req.body
+        strame = io.BytesIO(data)
+        p_data= JSONParser().parse(strame)
+        serializer=Stu_serializer(data=p_data)
+        if serializer.is_valid():
+            serializer.save()
+            print({'msg': 'object created'})
+            return HttpResponse({'msg':'object created'},content_type='application/json')
+           
+        else:
+            data=JSONRenderer().render(serializer.errors)
+            return HttpResponse(data,content_type='application/json')
+
+
     data = stu.objects.all()
     serializer = Stu_serializer(data,many=True)
     print(serializer)
     print(serializer.data)
+    # data = stu.objects.get(id=1)
+    # serializer = Stu_serializer(data)
+    # print(serializer)
+    # print(serializer.data)
     json = JSONRenderer().render(serializer.data)
     print(json)
     return HttpResponse(json,content_type='application/json')
